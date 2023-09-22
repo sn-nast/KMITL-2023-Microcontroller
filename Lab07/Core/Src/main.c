ed27f1024f042653b6c9870b66ae4cf5e82ecd2e
sn-nast <sunsun.natchanon@gmail.com>
2023-09-23 00:07:00 +0700

    lab07 rgb done - can't track pwm each color

--- Lab07/Core/Src/main.c
+++ Lab07/Core/Src/main.c
@@ -18,10 +18,15 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "stdio.h"
+#include "string.h"
+#include "stdarg.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -42,20 +47,228 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
+uint8_t pwm;
+uint8_t pwmR;
+uint8_t pwmG;
+uint8_t pwmB;
+
+float redIntensity = 0.0;
+float greenIntensity = 0.0;
+float blueIntensity = 0.0;
+
+typedef struct _Color
+{
+	TIM_HandleTypeDef * timer;
+	uint32_t timerChannel;
+	GPIO_TypeDef * pinGroup;
+	uint16_t pin;
+	float intensity;
+	uint8_t pwm;
+} Color;
+
+Color red, green, blue;
 
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-static void MPU_Initialize(void);
-static void MPU_Config(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+void generatePwm()
+{
+	HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_3);
+	HAL_Delay(100);
+	HAL_TIM_PWM_Stop(&htim2, TIM_CHANNEL_3);
+	pwm = (GPIOB->IDR & GPIO_PIN_10) >> 10;
+}
 
+void generatePwm2(float dutyCycle)
+{
+	htim2.Instance -> CCR3 = (10000-1) * dutyCycle;
+	HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_3);
+	HAL_Delay(100);
+	HAL_TIM_PWM_Stop(&htim2, TIM_CHANNEL_3);
+	pwm = (GPIOB->IDR & GPIO_PIN_10) >> 10;
+}
+
+void printOut(const char * text)
+{
+	while(__HAL_UART_GET_FLAG(&huart3,UART_FLAG_TC) == RESET) {}
+	HAL_UART_Transmit(&huart3, (uint8_t*) text, strlen(text), 100);
+}
+
+void printOutLine(const char * text)
+{
+	printOut(text);
+	printOut("\r\n");
+}
+
+void receiveUserInput(char *rxData)
+{
+	while(__HAL_UART_GET_FLAG(&huart3, UART_FLAG_RXNE) == RESET) {}
+	HAL_UART_Receive(&huart3, (uint8_t*) rxData, 1, 100);
+
+	printOutLine(rxData);
+}
+
+void addRedIntensity(float dutyCycle)
+{
+	htim3.Instance -> CCR1 = (10000-1) * dutyCycle;
+}
+
+void addGreenIntensity(float dutyCycle)
+{
+	htim3.Instance -> CCR2 = (10000-1) * dutyCycle;
+}
+
+void addBlueIntensity(float dutyCycle)
+{
+	htim3.Instance -> CCR3 = (10000-1) * dutyCycle;
+}
+
+void controlRgb(char *input)
+{
+	const float MAX_SCALE = 1.0;
+	const float STEP_SCALE = 0.1;
+
+	printOut("input char: ");
+	printOutLine(input);
+
+	if (*input == 'r' || *input == 'R')
+	{
+		if (redIntensity >= MAX_SCALE)
+		{
+			redIntensity = 0.0;
+		} else
+		{
+			redIntensity += STEP_SCALE;
+		}
+		addRedIntensity(redIntensity);
+	} else if (*input == 'g' || *input == 'G')
+	{
+		if (greenIntensity >= MAX_SCALE)
+		{
+			greenIntensity = 0.0;
+		} else
+		{
+			greenIntensity += STEP_SCALE;
+		}
+		addGreenIntensity(greenIntensity);
+	} else if (*input == 'b' || *input == 'B')
+	{
+		if (blueIntensity >= MAX_SCALE)
+		{
+			blueIntensity = 0.0;
+		} else
+		{
+			blueIntensity += STEP_SCALE;
+		}
+		addBlueIntensity(blueIntensity);
+	} else {
+		printOutLine("\t\t----SKIP----");
+	}
+
+	char text[50];
+	sprintf(text,"R: %.2f \t G: %.2f \t B: %.2f", redIntensity, greenIntensity, blueIntensity);
+	printOutLine(text);
+}
+
+void controlRgb2(char *input)
+{
+	const float MAX_SCALE = 1.0;
+	const float STEP_SCALE = 0.1;
+
+	printOut("input char: ");
+	printOutLine(input);
+
+	if (*input == 'r' || *input == 'R')
+	{
+		if (red.intensity >= MAX_SCALE)
+		{
+			red.intensity = 0.0;
+		} else
+		{
+			red.intensity += STEP_SCALE;
+		}
+		addRedIntensity(red.intensity);
+	} else if (*input == 'g' || *input == 'G')
+	{
+		if (green.intensity >= MAX_SCALE)
+		{
+			green.intensity = 0.0;
+		} else
+		{
+			green.intensity += STEP_SCALE;
+		}
+		addGreenIntensity(green.intensity);
+	} else if (*input == 'b' || *input == 'B')
+	{
+		if (blue.intensity >= MAX_SCALE)
+		{
+			blue.intensity = 0.0;
+		} else
+		{
+			blue.intensity += STEP_SCALE;
+		}
+		addBlueIntensity(blue.intensity);
+	} else {
+		printOutLine("\t\t----SKIP----");
+	}
+
+	char text[50];
+	sprintf(text,"R: %.2f \t G: %.2f \t B: %.2f", red.intensity, green.intensity, blue.intensity);
+	printOutLine(text);
+}
+
+void initColorType()
+{
+	red.timer = &htim3;
+	red.timerChannel = TIM_CHANNEL_1;
+	red.pinGroup = GPIOA;
+	red.pin = GPIO_PIN_6;
+	red.intensity = 0.0;
+	red.pwm = 0;
+
+	green.timer = &htim3;
+	green.timerChannel = TIM_CHANNEL_2;
+	green.pinGroup = GPIOA;
+	green.pin = GPIO_PIN_7;
+	green.intensity = 0.0;
+	green.pwm = 0;
+
+	blue.timer = &htim3;
+	blue.timerChannel = TIM_CHANNEL_3;
+	blue.pinGroup = GPIOC;
+	blue.pin = GPIO_PIN_8;
+	blue.intensity = 0.0;
+	blue.pwm = 0;
+}
+
+void startPwmRgb2()
+{
+//	HAL_TIM_PWM_Start(red.timer, red.timerChannel);
+//	HAL_TIM_PWM_Start(green.timer, green.timerChannel);
+//	HAL_TIM_PWM_Start(blue.timer, blue.timerChannel);
+
+	addRedIntensity(red.intensity);
+	addGreenIntensity(green.intensity);
+	addBlueIntensity(blue.intensity);
+}
+
+void startPwmRgb()
+{
+	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
+
+	addRedIntensity(redIntensity);
+	addGreenIntensity(greenIntensity);
+	addBlueIntensity(blueIntensity);
+}
 /* USER CODE END 0 */
 
 /**
@@ -65,16 +278,13 @@ static void MPU_Config(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
+	float dutyCycle = 0.5;
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
 
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-
-  /* MPU Configuration--------------------------------------------------------*/
-  MPU_Config();
 
   /* USER CODE BEGIN Init */
 
@@ -88,8 +298,16 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM2_Init();
+  MX_USART3_UART_Init();
+  MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
-
+  dutyCycle = 0.5;
+  char input;
+  initColorType();
+//  startPwmRgb();
+  startPwmRgb2();
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -99,6 +317,12 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+//	  generatePwm();
+//	  generatePwm2(dutyCycle);
+//	  controlRgb(&input);
+	  receiveUserInput(&input);
+	  controlRgb2(&input);
+
   }
   /* USER CODE END 3 */
 }
@@ -115,7 +339,7 @@ void SystemClock_Config(void)
   /** Configure the main internal regulator output voltage
   */
   __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
@@ -123,8 +347,21 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 216;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Activate the Over-Drive mode
+  */
+  if (HAL_PWREx_EnableOverDrive() != HAL_OK)
   {
     Error_Handler();
   }
@@ -133,12 +370,12 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK)
   {
     Error_Handler();
   }
@@ -147,35 +384,6 @@ void SystemClock_Config(void)
 /* USER CODE BEGIN 4 */
 
 /* USER CODE END 4 */
-
-/* MPU Configuration */
-
-void MPU_Config(void)
-{
-  MPU_Region_InitTypeDef MPU_InitStruct = {0};
-
-  /* Disables the MPU */
-  HAL_MPU_Disable();
-
-  /** Initializes and configures the Region and the memory to be protected
-  */
-  MPU_InitStruct.Enable = MPU_REGION_ENABLE;
-  MPU_InitStruct.Number = MPU_REGION_NUMBER0;
-  MPU_InitStruct.BaseAddress = 0x0;
-  MPU_InitStruct.Size = MPU_REGION_SIZE_4GB;
-  MPU_InitStruct.SubRegionDisable = 0x87;
-  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
-  MPU_InitStruct.AccessPermission = MPU_REGION_NO_ACCESS;
-  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
-  MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
-  MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
-  MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
-
-  HAL_MPU_ConfigRegion(&MPU_InitStruct);
-  /* Enables the MPU */
-  HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
-
-}
 
 /**
   * @brief  This function is executed in case of error occurrence.
